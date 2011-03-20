e5c38704fea2b3cd816dfb77d3bcafd85538469d
wyukawa <wyukawa@gmail.com>
2011-03-20 19:43:49 +0900

    p53

--- lib_misc.erl
+++ lib_misc.erl
@@ -1,5 +1,5 @@
 -module(lib_misc).
--export([sum/1, sum/2, for/3, qsort/1, pythag/1, perms/1]).
+-export([sum/1, sum/2, for/3, qsort/1, pythag/1, perms/1, odds_and_evens/1, odds_and_evens_acc/1]).
 
 sum(L) -> sum(L, 0).
 
@@ -25,3 +25,19 @@ pythag(N) ->
 
 perms([]) -> [[]];
 perms(L) -> [[H|T] || H <- L, T <- perms(L--[H])].
+
+odds_and_evens(L) ->
+  Odds = [X || X <- L, (X rem 2) =:= 1],
+  Evens = [X || X <- L, (X rem 2) =:= 0],
+  {Odds, Evens}.
+
+odds_and_evens_acc(L) ->
+  odds_and_evens_acc(L, [], []).
+
+odds_and_evens_acc([H|T], Odds, Evens) ->
+  case (H rem 2) of
+    1 -> odds_and_evens_acc(T, [H|Odds], Evens);
+    0 -> odds_and_evens_acc(T, Odds, [H|Evens])
+  end;
+odds_and_evens_acc([], Odds, Evens) ->
+  {Odds, Evens}.
