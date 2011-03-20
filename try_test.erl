09920e563e1532b4e69804ce7046bfb9990b875d
wyukawa <wyukawa@gmail.com>
2011-03-20 20:44:53 +0900

    p59

---
+++ try_test.erl
@@ -0,0 +1,20 @@
+-module(try_test).
+-export([demo1/0]).
+
+demo1() ->
+  [catcher(I) || I <- [1,2,3,4,5]].
+
+catcher(N) ->
+  try generate_exception(N) of
+      Val -> {N, normal, Val}
+  catch
+      throw:X -> {N, caught, thrown, X};
+      exit:X -> {N, caught, exited, X};
+      error:X -> {N, caught, error, X}
+  end.
+
+generate_exception(1) -> a;
+generate_exception(2) -> throw(a);
+generate_exception(3) -> exit(a);
+generate_exception(4) -> {'Exit',a};
+generate_exception(5) -> erlang:error(a).
