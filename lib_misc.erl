4a1c306f978f2707de150e8f9546ed54d60c7176
wyukawa <wyukawa@gmail.com>
2011-03-20 17:58:37 +0900

    p38

--- lib_misc.erl
+++ lib_misc.erl
@@ -1,7 +1,10 @@
 -module(lib_misc).
--export([sum/1, sum/2]).
+-export([sum/1, sum/2, for/3]).
 
 sum(L) -> sum(L, 0).
 
 sum([], N) -> N;
 sum([H|T], N) -> sum(T, H+N).
+
+for(Max, Max, F) -> [F(Max)];
+for(I, Max, F) -> [F(I)|for(I+1, Max, F)].
