60022264559310c0a33950bf240f6037a3943ce4
wyukawa <wyukawa@gmail.com>
2011-03-20 18:03:06 +0900

    p43

--- lib_misc.erl
+++ lib_misc.erl
@@ -1,5 +1,5 @@
 -module(lib_misc).
--export([sum/1, sum/2, for/3]).
+-export([sum/1, sum/2, for/3, qsort/1]).
 
 sum(L) -> sum(L, 0).
 
@@ -8,3 +8,8 @@ sum([H|T], N) -> sum(T, H+N).
 
 for(Max, Max, F) -> [F(Max)];
 for(I, Max, F) -> [F(I)|for(I+1, Max, F)].
+
+qsort([]) -> [];
+qsort([Pivot|T]) -> qsort([X || X <- T, X < Pivot])
+                    ++ [Pivot] ++
+                    qsort([X || X <- T, X >= Pivot]).
